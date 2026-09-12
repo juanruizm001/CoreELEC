54f2c3b515fd7d2a0ad3df0451bad4cc99e1253f
Juan Ruiz <6059278+juanruizm001@users.noreply.github.com>
2026-09-12 18:40:32 -0300

    linux: build Amlogic-ce from final SMB multichannel submission

--- projects/Amlogic-ce/packages/linux/package.mk
+++ projects/Amlogic-ce/packages/linux/package.mk
@@ -4,11 +4,11 @@
 # Copyright (C) 2024-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="linux"
-PKG_VERSION="69e87fa40cc3a04bf91d92e388d2787a2efcb50c"
+PKG_VERSION="3294f2c166d2ce27d523f84243763b57e5641352"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kernel.org"
-PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/juanruizm001/linux-amlogic/archive/${PKG_VERSION}.tar.gz"
 PKG_GIT_BRANCH="5.15.196_20260306"
 PKG_BUILD_PERF="no"
 PKG_DEPENDS_HOST="ccache:host rsync:host openssl:host"
